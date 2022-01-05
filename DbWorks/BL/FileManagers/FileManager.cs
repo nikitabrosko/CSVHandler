131ce4fbefc1c582444fc2d050264bc23b7e6fe2
Nik.Brosko <Nikita.brosko@mail.ru>
2022-01-05 14:48:34 +0300

    Add FileManager class

---
+++ DbWorks/BL/FileManagers/FileManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using BL.Abstractions;
+
+namespace BL.FileManagers
+{
+    public class FileManager : IFileManager
+    {
+        public event EventHandler<FileSystemEventArgs> FileIsAdd;
+
+        private readonly FileSystemWatcher _fileSystemWatcher;
+        private readonly string _directoryPath;
+
+        public bool IsDisposed;
+        
+        public FileManager(string directoryPath, string filesToWatchExtension)
+        {
+            _directoryPath = directoryPath;
+
+            _fileSystemWatcher = new FileSystemWatcher(directoryPath)
+            {
+                Filter = filesToWatchExtension
+            };
+
+            _fileSystemWatcher.Created += OnCreated;
+
+            _fileSystemWatcher.EnableRaisingEvents = true;
+        }
+
+        public void Stop()
+        {
+            _fileSystemWatcher.Created -= OnCreated;
+        }
+
+        public void MoveFileToAnotherDirectory(string targetDirectoryPath, string fileName)
+        {
+            File.Move(_directoryPath + fileName, targetDirectoryPath + fileName);
+            File.Delete(_directoryPath + fileName);
+        }
+
+        protected virtual void OnCreated(object sender, FileSystemEventArgs args)
+        {
+            FileIsAdd?.Invoke(this, args);
+        }
+        
+        public void Dispose()
+        {
+            _fileSystemWatcher.Created -= OnCreated;
+            _fileSystemWatcher?.Dispose();
+            IsDisposed = true;
+            GC.SuppressFinalize(this);
+        }
+    }
+}
