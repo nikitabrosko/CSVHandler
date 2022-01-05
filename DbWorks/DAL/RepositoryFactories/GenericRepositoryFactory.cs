1866bfc8696c40db106ff638fa3f5a98cfea0d59
Nik.Brosko <Nikita.brosko@mail.ru>
2022-01-05 22:38:53 +0300

    Rename RepositoryFactory

--- DbWorks/DAL/RepositoryFactories/GenericRepositoryFactory.cs
+++ DbWorks/DAL/RepositoryFactories/GenericRepositoryFactory.cs
@@ -1,10 +1,11 @@
 ﻿using System.Data.Entity;
 using DAL.Abstractions;
+using DAL.Abstractions.Factories;
 using DAL.Repositories;
 
 namespace DAL.RepositoryFactories
 {
-    public class RepositoryFactory : IRepositoryFactory
+    public class GenericRepositoryFactory : IGenericRepositoryFactory
     {
         public IGenericRepository<TEntity> CreateInstance<TEntity>(DbContext context) where TEntity : class
         {
