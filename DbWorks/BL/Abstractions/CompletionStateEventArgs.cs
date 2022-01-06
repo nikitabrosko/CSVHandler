e9e7a0e701df46a6b0593ffdb057a41af9748a47
Nik.Brosko <Nikita.brosko@mail.ru>
2022-01-06 18:29:10 +0300

    Create completion state enum and event args

---
+++ DbWorks/BL/Abstractions/CompletionStateEventArgs.cs
@@ -0,0 +1,15 @@
+﻿namespace BL.Abstractions
+{
+    public class CompletionStateEventArgs
+    {
+        public CompletionState CompletionState { get; }
+        
+        public string FileName { get; }
+
+        public CompletionStateEventArgs(CompletionState completionState, string fileName)
+        {
+            CompletionState = completionState;
+            FileName = fileName;
+        }
+    }
+}
