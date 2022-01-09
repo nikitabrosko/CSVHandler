21141bb37fca9113b4340cdeacae44c0fd9ee7b1
Nik.Brosko <Nikita.brosko@mail.ru>
2022-01-09 19:16:00 +0300

    Add tests to SalesDataSourceDTOs

---
+++ DbWorks/BLTests/SalesDataSourceDTOsTests/SalesDataSourceDTOTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using BL.SalesDataSourceDTOs;
+using DbWorks.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace BLTests.SalesDataSourceDTOsTests
+{
+    [TestClass]
+    public class SalesDataSourceDTOTests
+    {
+        [TestMethod]
+        public void CreatingSalesDataSourceDtoWithInvalidParametersCustomerIsNull()
+        {
+            Customer customer = null;
+            var manager = new Mock<Manager>().Object;
+            var order = new Mock<Order>().Object;
+            var product = new Mock<Product>().Object;
+
+            Assert.ThrowsException<ArgumentNullException>(() =>
+                new SalesDataSourceDTO(customer, manager, order, product));
+        }
+
+        [TestMethod]
+        public void CreatingSalesDataSourceDtoWithInvalidParametersManagerIsNull()
+        {
+            var customer = new Mock<Customer>().Object;
+            Manager manager = null;
+            var order = new Mock<Order>().Object;
+            var product = new Mock<Product>().Object;
+
+            Assert.ThrowsException<ArgumentNullException>(() =>
+                new SalesDataSourceDTO(customer, manager, order, product));
+        }
+
+        [TestMethod]
+        public void CreatingSalesDataSourceDtoWithInvalidParametersOrderIsNull()
+        {
+            var customer = new Mock<Customer>().Object;
+            var manager = new Mock<Manager>().Object;
+            Order order = null;
+            var product = new Mock<Product>().Object;
+
+            Assert.ThrowsException<ArgumentNullException>(() =>
+                new SalesDataSourceDTO(customer, manager, order, product));
+        }
+
+        [TestMethod]
+        public void CreatingSalesDataSourceDtoWithInvalidParametersProductIsNull()
+        {
+            var customer = new Mock<Customer>().Object;
+            var manager = new Mock<Manager>().Object;
+            var order = new Mock<Order>().Object;
+            Product product = null;
+
+            Assert.ThrowsException<ArgumentNullException>(() =>
+                new SalesDataSourceDTO(customer, manager, order, product));
+        }
+
+        [TestMethod]
+        public void CreatingSalesDataSourceDtoWithValidParameters()
+        {
+            var customer = new Customer();
+            var manager = new Manager();
+            var order = new Order();
+            var product = new Product();
+
+            var salesDataSourceDtoObject = new SalesDataSourceDTO(customer, manager, order, product);
+
+            Assert.IsTrue(salesDataSourceDtoObject.Customer.Equals(customer)
+                          && salesDataSourceDtoObject.Manager.Equals(manager)
+                          && salesDataSourceDtoObject.Order.Equals(order)
+                          && salesDataSourceDtoObject.Product.Equals(product));
+        }
+    }
+}
---
+++ DbWorks/BLTests/SalesDataSourceDTOsTests/SalesDataSourceHandlerTests.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using BL.Abstractions;
+using BL.SalesDataSourceDTOs;
+using DbWorks.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BLTests.SalesDataSourceDTOsTests
+{
+    [TestClass]
+    public class SalesDataSourceHandlerTests
+    {
+        [TestMethod]
+        [DataRow(null, "Test", "Test", "Test", "5", "01012022", "20")]
+        [DataRow("Test", null, "Test", "Test", "5", "01012022", "20")]
+        [DataRow("Test", "Test", null, "Test", "5", "01012022", "20")]
+        [DataRow("Test", "Test", "Test", null, "5", "01012022", "20")]
+        [DataRow("Test", "Test", "Test", "Test", null, "01012022", "20")]
+        [DataRow("Test", "Test", "Test", "Test", "5", null, "20")]
+        [DataRow("Test", "Test", "Test", "Test", "5", "01012022", null)]
+        public void SalesDataSourceHandlerCreatingWithInvalidParametersCustomerFirstNameIsNullTest(string customerFirstName,
+            string customerLastName, string managerLastName, string productName, string productPrice, string orderDate, string orderSum)
+        {
+            Assert.ThrowsException<ArgumentNullException>(() =>
+                new SalesDataSourceHandler(customerFirstName, customerLastName, managerLastName, 
+                    productName, productPrice, orderDate, orderSum));
+        }
+
+        [TestMethod]
+        [DataRow("Test", "Test", "Test", "Test", "5", "01.01.2022", "20")]
+        public void SalesDataSourceHandlerGetSalesDataSourceDtoMethodTest(string customerFirstName, string customerLastName, 
+            string managerLastName, string productName, string productPrice, string orderDate, string orderSum)
+        {
+            var salesDataSourceHandler = new SalesDataSourceHandler(customerFirstName, customerLastName, 
+                managerLastName, productName, productPrice, orderDate, orderSum);
+
+            var customer = new Customer
+            {
+                FirstName = customerFirstName, 
+                LastName = customerLastName
+            };
+
+            var manager = new Manager
+            {
+                LastName = managerLastName
+
+            };
+
+            var product = new Product
+            {
+                Name = productName, 
+                Price = decimal.Parse(productPrice)
+            };
+
+            var order = new Order
+            {
+                Customer = customer,
+                Manager = manager,
+                Product = product,
+                Date = DateTime.Parse(orderDate),
+                Sum = decimal.Parse(orderSum)
+            };
+
+            var expectedDataSourceDto = new SalesDataSourceDTO(customer, manager, order, product);
+
+            var actualDataSourceDto = salesDataSourceHandler.GetSalesDataSourceDTO();
+
+            Assert.IsTrue(CheckTwoSalesDataSourcesDto(expectedDataSourceDto, actualDataSourceDto));
+        }
+
+        private static bool CheckTwoSalesDataSourcesDto(ISalesDataSourceDTO firstDataSource, ISalesDataSourceDTO secondDataSource)
+        {
+            var checkForCustomerEquals =
+                Equals(firstDataSource.Customer.FirstName, secondDataSource.Customer.FirstName)
+                && Equals(firstDataSource.Customer.LastName, secondDataSource.Customer.LastName)
+                && Equals(firstDataSource.Customer.FullName, secondDataSource.Customer.FullName)
+                && Equals(firstDataSource.Customer.Id, secondDataSource.Customer.Id)
+                && Equals(firstDataSource.Customer.Orders.ToString(), secondDataSource.Customer.Orders.ToString());
+
+            var checkForManagerEquals =
+                Equals(firstDataSource.Manager.Id, secondDataSource.Manager.Id)
+                && Equals(firstDataSource.Manager.LastName, secondDataSource.Manager.LastName)
+                && Equals(firstDataSource.Manager.Orders.ToString(), secondDataSource.Manager.Orders.ToString());
+
+            var checkForProductEquals =
+                Equals(firstDataSource.Product.Id, secondDataSource.Product.Id)
+                && Equals(firstDataSource.Product.Name, secondDataSource.Product.Name)
+                && Equals(firstDataSource.Product.Orders.ToString(), secondDataSource.Product.Orders.ToString())
+                && Equals(firstDataSource.Product.Price, secondDataSource.Product.Price);
+
+            var checkForOrderEquals =
+                Equals(firstDataSource.Order.Customer.ToString(), secondDataSource.Order.Customer.ToString())
+                && Equals(firstDataSource.Order.Manager.ToString(), secondDataSource.Order.Manager.ToString())
+                && Equals(firstDataSource.Order.Product.ToString(), secondDataSource.Order.Product.ToString())
+                && Equals(firstDataSource.Order.Date, secondDataSource.Order.Date)
+                && Equals(firstDataSource.Order.Id, secondDataSource.Order.Id)
+                && Equals(firstDataSource.Order.Sum, secondDataSource.Order.Sum);
+
+            return checkForCustomerEquals
+                   && checkForManagerEquals
+                   && checkForProductEquals
+                   && checkForOrderEquals;
+        }
+    }
+}
