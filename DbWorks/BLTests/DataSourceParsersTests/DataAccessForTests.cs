6b8e1420d394fdd37774df999fd66f4e869336c5
Nik.Brosko <Nikita.brosko@mail.ru>
2022-01-09 19:14:46 +0300

    Add file for tests Add config json file Add class for access to data

---
+++ DbWorks/BLTests/DataSourceParsersTests/DataAccessForTests.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace BLTests.DataSourceParsersTests
+{
+    public static class DataAccessForTests
+    {
+        public static string PathToFolder =>
+            new ConfigurationBuilder()
+                .AddJsonFile(Path.GetFullPath(@"..\\..\\..\\DataSourceParsersTests\\TestsSettings.json"))
+                .Build()
+                .GetSection("TestFilesFolders:FilesForTests").Value;
+
+        public static string FileName => "Test_01012022.csv";
+
+        public static string FileText => "01012022;Ivan Sidorov;Telephone, 5;20";
+
+        public static void CreateFileIfNotExists()
+        {
+            var fullPath = PathToFolder + FileName;
+
+            if (!File.Exists(fullPath))
+            {
+                File.Create(fullPath);
+                File.WriteAllText(fullPath, FileText);
+            }
+        }
+    }
+}
