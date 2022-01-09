c523949db704070607502db428b1ffa3f1735344
Nik.Brosko <Nikita.brosko@mail.ru>
2022-01-09 19:12:05 +0300

    Create ISalesDataSourceHandler interface

---
+++ DbWorks/BL/Abstractions/ISalesDataSourceHandler.cs
@@ -0,0 +1,14 @@
+﻿namespace BL.Abstractions
+{
+    public interface ISalesDataSourceHandler
+    {
+        string CustomerFirstName { get; }
+        string CustomerLastName { get; }
+        string ManagerLastName { get; }
+        string ProductName { get; }
+        string ProductPrice { get; }
+        string OrderDate { get; }
+        string OrderSum { get; }
+        ISalesDataSourceDTO GetSalesDataSourceDTO();
+    }
+}
