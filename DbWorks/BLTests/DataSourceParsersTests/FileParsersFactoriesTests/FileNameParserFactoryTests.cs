1ded1fcdce4c9f8096dcae8c236361e501859a6e
Nik.Brosko <Nikita.brosko@mail.ru>
2022-01-09 19:15:21 +0300

    Add tests for DataSourceParsers

---
+++ DbWorks/BLTests/DataSourceParsersTests/FileParsersFactoriesTests/FileNameParserFactoryTests.cs
@@ -0,0 +1,23 @@
+﻿using BL.DataSourceParsers.FileParsers;
+using BL.DataSourceParsers.FileParsersFactories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BLTests.DataSourceParsersTests.FileParsersFactoriesTests
+{
+    [TestClass]
+    public class FileNameParserFactoryTests
+    {
+        [TestMethod]
+        public void FileNameParserFactoryTestCreateInstanceMethod()
+        {
+            DataAccessForTests.CreateFileIfNotExists();
+
+            var fileNameParserFactory = new FileNameParserFactory();
+
+            var expectedFileNameParserObject = new FileNameParser(DataAccessForTests.FileName);
+            var actualFileNameParserObject = fileNameParserFactory.CreateInstance(DataAccessForTests.FileName);
+
+            Assert.AreEqual(expectedFileNameParserObject.ToString(), actualFileNameParserObject.ToString());
+        }
+    }
+}
---
+++ DbWorks/BLTests/DataSourceParsersTests/FileParsersFactoriesTests/FileParserFactoryTests.cs
@@ -0,0 +1,25 @@
+﻿using BL.DataSourceParsers.FileParsers;
+using BL.DataSourceParsers.FileParsersFactories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BLTests.DataSourceParsersTests.FileParsersFactoriesTests
+{
+    [TestClass]
+    public class FileParserFactoryTests
+    {
+        [TestMethod]
+        public void FileParserFactoryTestCreateInstanceMethod()
+        {
+            DataAccessForTests.CreateFileIfNotExists();
+
+            var fileParserFactory = new FileParserFactory();
+
+            var expectedFileParserObject =
+                new FileParser(DataAccessForTests.PathToFolder + DataAccessForTests.FileName);
+            var actualFileParserObject =
+                fileParserFactory.CreateInstance(DataAccessForTests.PathToFolder + DataAccessForTests.FileName);
+
+            Assert.AreEqual(expectedFileParserObject.ToString(), actualFileParserObject.ToString());
+        }
+    }
+}
