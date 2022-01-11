28edde387c3b2864ee13a4bed703ca9c283abcdb
Nik.Brosko <Nikita.brosko@mail.ru>
2022-01-11 19:04:50 +0300

    Add new tests classes

---
+++ DbWorks/BLTests/EventArgsTests/CompletionStateEventArgsTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using BL.Abstractions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BLTests.EventArgsTests
+{
+    [TestClass]
+    public class CompletionStateEventArgsTests
+    {
+        [TestMethod]
+        public void CompletionStateEventArgsCreatingWithValidParameterTest()
+        {
+            var fileName = "TestName";
+
+            var eventArgs = new CompletionStateEventArgs(CompletionState.Completed, fileName);
+
+            Assert.IsTrue(Equals(fileName, eventArgs.FileName)
+                          && Equals(CompletionState.Completed, eventArgs.CompletionState));
+        }
+
+        [TestMethod]
+        public void CompletionStateEventArgsCreatingWithInvalidParameterTest()
+        {
+            string fileName = null;
+
+            Assert.ThrowsException<ArgumentNullException>(() =>
+                new CompletionStateEventArgs(CompletionState.Completed, fileName));
+        }
+    }
+}
---
+++ DbWorks/BLTests/FileManagersTests/FileManagerTests.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.IO;
+using BL.FileManagers;
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BLTests.FileManagersTests
+{
+    [TestClass]
+    public class FileManagerTests
+    {
+        private readonly string _sourceDirectoryPath =
+            new ConfigurationBuilder()
+                .AddJsonFile(Path.GetFullPath(@"..\\..\\..\\FileManagersTests\\TestsSettings.json"))
+                .Build()
+                .GetSection("TestFilesFolders:SourceFolder").Value;
+
+        private readonly string _targetDirectoryPath =
+            new ConfigurationBuilder()
+                .AddJsonFile(Path.GetFullPath(@"..\\..\\..\\FileManagersTests\\TestsSettings.json"))
+                .Build()
+                .GetSection("TestFilesFolders:TargetFolder").Value;
+
+        private readonly string _fileExtension =
+            new ConfigurationBuilder()
+                .AddJsonFile(Path.GetFullPath(@"..\\..\\..\\FileManagersTests\\TestsSettings.json"))
+                .Build().GetSection("FilesExtensions:FilesExtensionForTests").Value;
+
+        [TestMethod]
+        [DataRow("", "testPath")]
+        [DataRow(" ", "testPath")]
+        [DataRow(null, "testPath")]
+        [DataRow("testPath", "")]
+        [DataRow("testPath", " ")]
+        [DataRow("testPath", null)]
+        public void FileManagerCreatingWithInvalidParametersTest(string directoryPath, string filesToWatchExtension)
+        {
+            Assert.ThrowsException<ArgumentException>(() => 
+                    new FileManager(directoryPath, filesToWatchExtension),
+                "argument is null, empty or whitespace");
+        }
+
+        [TestMethod]
+        public void FileManagerDisposeMethodTest()
+        {
+            var fileManager = new FileManager(_sourceDirectoryPath, _fileExtension);
+
+            fileManager.Dispose();
+
+            Assert.IsTrue(fileManager.IsDisposed);
+        }
+
+        [TestMethod]
+        public void FileManagerMoveFileToAnotherDirectoryMethodTest()
+        {
+            var fileName = "TestFirst_01012022.csv";
+
+            if (!File.Exists(_sourceDirectoryPath + fileName))
+            {
+                File.CreateText(_sourceDirectoryPath + fileName).Close();
+            }
+
+            var fileManager = new FileManager(_sourceDirectoryPath, _fileExtension);
+            fileManager.MoveFileToAnotherDirectory(_targetDirectoryPath, fileName);
+
+            Assert.IsTrue(File.Exists(_targetDirectoryPath + fileName));
+
+            File.Delete(_targetDirectoryPath + fileName);
+            fileManager.Dispose();
+        }
+
+        [TestMethod]
+        public void FileManagerMoveFileToAnotherDirectoryMethodWithFileExistsTest()
+        {
+            var fileName = "TestSecond_01012022.csv";
+
+            if (!File.Exists(_sourceDirectoryPath + fileName))
+            {
+                File.CreateText(_sourceDirectoryPath + fileName).Close();
+            }
+
+            var fileManager = new FileManager(_sourceDirectoryPath, _fileExtension);
+
+            if (!File.Exists(_targetDirectoryPath + fileName))
+            {
+                File.CreateText(_targetDirectoryPath + fileName).Close();
+            }
+
+            fileManager.MoveFileToAnotherDirectory(_targetDirectoryPath, fileName);
+
+            Assert.IsTrue(File.Exists(_targetDirectoryPath + fileName));
+
+            File.Delete(_targetDirectoryPath + fileName);
+            fileManager.Dispose();
+        }
+    }
+}
